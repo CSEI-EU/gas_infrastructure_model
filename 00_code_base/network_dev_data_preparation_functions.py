c5a08a628ff86de3d597b874550e27bea03b19ca
Johannes Giehl <hannesfelipe@gmail.com>
2024-02-01 16:19:27 +0100

    move functions to separate script

---
+++ 00_code_base/network_dev_data_preparation_functions.py
@@ -0,0 +1,53 @@
+'''
+This is the network development of CSEI, an Open-source Tool
+for investigating the development of infrastrucutre. The focus of the tool is on
+the development of the hydrogen infrastructure. 
+The tool is developed at the Copenhagen School of
+Energy Infrastructure at the Copenhagen Business School.
+---------------------------------------
+Functions File for Data Prep:
+The functions include the logical prossecing, mathematical calculation
+logic for the preparation of the input data file for the model runs.
+SPDX-FileCopyrightText: Johannes Giehl <jfg.eco@cbs.dk>
+SPDX-License-Identifier: GNU GENERAL PUBLIC LICENSE GPL 3.0
+'''
+
+'''Import packages'''
+import numpy as np
+import pandas as pd
+
+'''Define functions'''
+
+def generate_parallel_connections(input_df):
+    result_rows = []
+
+    for _, row in input_df.iterrows():
+        source_name = row['source_name']
+        target_name = row['target_name']
+        occurrences = row['occurrences']
+
+        for i in range(1, occurrences + 1):
+            new_target = f"{target_name}_{i}"
+            result_rows.append({'source_name': source_name, 'target_name': new_target})
+
+            # Add a source node targeting the original target node
+            result_rows.append({'source_name': f"{new_target}", 'target_name': target_name})
+
+    result_df = pd.DataFrame(result_rows)
+
+    return result_df
+
+def replace_nan_with_value(dataframe, column_name, replacement_value):
+    """
+    Replace all NaN values in a specific column of a DataFrame with a given value.
+
+    Parameters:
+        dataframe (pd.DataFrame): The input DataFrame.
+        column_name (str): The name of the column in which NaN values should be replaced.
+        replacement_value: The value to replace NaN with.
+
+    Returns:
+        pd.DataFrame: A new DataFrame with NaN values replaced in the specified column.
+    """
+    dataframe.fillna({column_name: replacement_value}, inplace=True)
+    return dataframe
