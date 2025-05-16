4bfb0caf3447b3393c14c20fe7c4acd03cfef1a5
Flora <flv.eco@cbs.dk>
2025-05-16 16:47:14 +0200

    Updated output procdure - saving of plots works

--- 00_code_base/08_output_preparation/plot_visualization.py
+++ 00_code_base/08_output_preparation/plot_visualization.py
@@ -16,18 +16,18 @@ base_path =r"C:\Users\flv.eco\OneDrive - CBS - Copenhagen Business School\Docume
 #
 
 # Import this for save funcroin to work
-# pip install -U kaleido
-save_flow_invest = True
+# pip install kaleido==0.1.0post1
+save_flow_invest = False
 save_flow_no_invest = True
 file_name_invest = "Cross_border_flow_invest.png"
-file_name_no_invest = "Cross_border_flow_noinvest.png"
+file_name_no_invest = "Cross_border_flow_2021.png"
 
-output_path_no_invest = os.path.join(base_path, "02_plots", "Flow_plots", file_name_no_invest)
-output_path_invest = os.path.join(base_path, "02_plots", "Flow_plots", file_name_invest)
+output_path_no_invest = os.path.join(base_path, "02_plots", "Flow_Results", file_name_no_invest)
+output_path_invest = os.path.join(base_path, "02_plots", "Flow_Results", file_name_invest)
 
 
 input_LNG_file = os.path.join(base_path, "01_data", "01_input_data", "01_raw", "01_Russian_War_Case", "LNG_locations.xlsx")
-output_file_no_invest = os.path.join(base_path, "01_data", "02_output_data", "02_unidirectional_results", "01_paper_IAEE", "01_raw_results", "outputs_IAEE_2025_run_2024.xlsx")
+output_file_no_invest = os.path.join(base_path, "01_data", "02_output_data", "02_unidirectional_results", "01_paper_IAEE", "01_raw_results", "outputs_IAEE_2025_run_2021.xlsx")
 output_file_invest = os.path.join(base_path, "01_data", "02_output_data", "02_unidirectional_results", "01_paper_IAEE", "01_raw_results", "outputs_IAEE_2025_run_2024_inv.xlsx")
 output_2021 = os.path.join(base_path, "01_data", "02_output_data", "02_unidirectional_results", "01_paper_IAEE", "02_prepared_results", "output_2021_prepared.xlsx")
 output_2024 = os.path.join(base_path, "01_data", "02_output_data", "02_unidirectional_results", "01_paper_IAEE", "02_prepared_results", "output_2024_prepared.xlsx")
@@ -104,28 +104,29 @@ fig = plot_flow_map(df_with_coords, filtered_ports, df_with_imports, title)
 
 #Save the figure
 if save_flow_no_invest: 
-    fig.write_image(output_path_no_invest, width=1200, height=800, scale=2)
+    df_with_imports.to_excel(output_path_no_invest.replace('.png', '_utilization_share.xlsx'), index=False)
+    fig.write_image(output_path_no_invest, width=1135, height=800, scale=2)
 
 # ---------------------------------------------------------------------
 # Do all the same for the investment case 
-df_invest = pd.read_excel(output_file_invest)
-df_invest = df_invest[df_invest['Commodity']=='Methane']
+# df_invest = pd.read_excel(output_file_invest)
+# df_invest = df_invest[df_invest['Commodity']=='Methane']
 
-df_invest_raw = parse_edges(df_invest)
-df_invest_raw['FromType'] = df_invest_raw['From'].apply(label_node_type)
-df_invest_raw['ToType'] = df_invest_raw['To'].apply(label_node_type)
-df_invest_raw['From'] = df_invest_raw['From'].apply(extract_country_code)
-df_invest_raw['To'] = df_invest_raw['To'].apply(extract_country_code)
+# df_invest_raw = parse_edges(df_invest)
+# df_invest_raw['FromType'] = df_invest_raw['From'].apply(label_node_type)
+# df_invest_raw['ToType'] = df_invest_raw['To'].apply(label_node_type)
+# df_invest_raw['From'] = df_invest_raw['From'].apply(extract_country_code)
+# df_invest_raw['To'] = df_invest_raw['To'].apply(extract_country_code)
 
-# Filter the rows based on the countries of interest (same as before)
-df_invest_filtered = df_invest_raw[df_invest_raw['From'].apply(interesting_countries) & df_invest_raw['To'].apply(european_countries)]
-df_invest_capacity = add_capacity_column(df_invest_filtered, output_file_invest)
-df_invest_with_share = add_share_column(df_invest_capacity)
+# # Filter the rows based on the countries of interest (same as before)
+# df_invest_filtered = df_invest_raw[df_invest_raw['From'].apply(interesting_countries) & df_invest_raw['To'].apply(european_countries)]
+# df_invest_capacity = add_capacity_column(df_invest_filtered, output_file_invest)
+# df_invest_with_share = add_share_column(df_invest_capacity)
 
-# Add coordinates 
-df_invest_with_coords = add_coordinates(df_invest_with_share)
+# # Add coordinates 
+# df_invest_with_coords = add_coordinates(df_invest_with_share)
 
 
-# Plot the flow map for the "investment" scenario
-title_invest = "Cross-border NG flows with investment (2024)"
-fig = plot_flow_map(df_invest_with_coords, filtered_ports, df_with_imports, title = title_invest)
+# # Plot the flow map for the "investment" scenario
+# title_invest = "Cross-border NG flows with investment (2024)"
+# fig = plot_flow_map(df_invest_with_coords, filtered_ports, df_with_imports, title = title_invest)
