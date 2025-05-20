4b3a4e50c07eb9d5e77b5e50ea8a9299464847c1
mathilderoger <mathilde.roger@ensta-paris.fr>
2025-05-20 13:22:28 +0200

    Updates on heatmaps and cost difference

--- 00_code_base/08_output_preparation/plot_visualization.py
+++ 00_code_base/08_output_preparation/plot_visualization.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-os.chdir(r"C:\Users\flv.eco\OneDrive - CBS - Copenhagen Business School\Documents\03_LNG_Cap\hydrogen_grid\00_code_base\08_output_preparation")
+# os.chdir(r"C:\Users\flv.eco\OneDrive - CBS - Copenhagen Business School\Documents\03_LNG_Cap\hydrogen_grid\00_code_base\08_output_preparation")
 from output_visualization_functions import *
 from LNG_external_imports_functions import *
 from year_difference_functions import *
 
-base_path =r"C:\Users\flv.eco\OneDrive - CBS - Copenhagen Business School\Documents\03_LNG_Cap\hydrogen_grid"
-# base_path = r"C:\Users\mar.eco\OneDrive - CBS - Copenhagen Business School\Desktop\hydrogen_grid"
+# base_path =r"C:\Users\flv.eco\OneDrive - CBS - Copenhagen Business School\Documents\03_LNG_Cap\hydrogen_grid"
+base_path = r"C:\Users\mar.eco\OneDrive - CBS - Copenhagen Business School\Desktop\hydrogen_grid"
 #
 
 # Import this for save funcroin to work
@@ -217,13 +217,13 @@ if save_flow_invest:
 
 
 data_path = os.path.join(base_path, "01_data", "02_output_data", "02_unidirectional_results", "01_paper_IAEE", "02_prepared_results")
-scenario = "2024_plus_no_USA"
-full_name = "costs_shares_IAEE_2025_run_2024_plus_no_USA"
-input_file_cost = os.path.join(data_path, "costs_shares_IAEE_2025_run_2024_plus_no_USA.xlsx")
+scenario = "2035_AP_to_SP"
+full_name = "costs_shares_IAEE_2025_run_2035_AP"
+input_file_cost = os.path.join(data_path, "costs_shares_IAEE_2025_run_2035_AP.xlsx")
 
 file_cost_difference_2021 = os.path.join(data_path, "cost_shares_differences_to_2021.xlsx")
 file_cost_difference_2024 = os.path.join(data_path, "cost_shares_differences_to_2024.xlsx")
-ile_cost_difference_2035 = os.path.join(data_path, "cost_shares_differences_to_2035_SP.xlsx")
+file_cost_difference_2035 = os.path.join(data_path, "cost_shares_differences_to_2035_SP.xlsx")
 
 # plot_cost_map(input_file_cost, scenario, base_path, False)
-plot_cost_difference(file_cost_difference_2024, full_name, scenario, base_path, True)
+plot_cost_difference(file_cost_difference_2035, full_name, scenario, base_path, True)
